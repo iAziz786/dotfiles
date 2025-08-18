9fbfdb5e4ea200a3b70aa7362d2816cee1c37381
Mohammad Aziz <mdaziz067@gmail.com>
2025-08-18 09:30:07 +0530

    Run zoxide's fuzzy search with fzf when Ctrl + f

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require("wezterm")
-
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 16
@@ -10,5 +10,10 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 10
+
+-- Keybinding Ctrl + f runs zoxide's fzf jumper (zi) in the current pane
+config.keys = {
+	{ key = "f", mods = "CTRL", action = act.SendString("zi\n") },
+}
 
 return config
