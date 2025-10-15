042547de1b7e3c87750f773392b71023f32bca29
Mohammad Aziz <mdaziz067@gmail.com>
2025-10-15 07:06:50 +0530

    Fix catpuccin error of bufferline

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -13,16 +13,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-
-  -- https://github.com/LazyVim/LazyVim/pull/6354
-  -- For now let's use the work around for catppuccin buffline breaking right now
-  {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
 }
